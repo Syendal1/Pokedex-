2205222dd7a8c90ad99f927829412615a2ace7ad
Syendal1 <snehith.yendal0313@gmail.com>
2016-08-16 00:01:45 -0700

    Putting all the Pokemons

--- Pokedex/PokeCell.swift
+++ Pokedex/PokeCell.swift
@@ -14,6 +14,12 @@ class PokeCell: UICollectionViewCell {
     
     var pokemon:Pokemon!
     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius=8
+    }
+    
     func configureCell(pokemon:Pokemon){
         self.pokemon=pokemon
         
