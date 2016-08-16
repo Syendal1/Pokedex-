8ce72d062af13e2ba8b4abf6dd237c4dda39304d
Syendal1 <snehith.yendal0313@gmail.com>
2016-08-16 14:09:10 -0700

    Names assign Done

--- Pokedex/PokeCell.swift
+++ Pokedex/PokeCell.swift
@@ -12,19 +12,21 @@ class PokeCell: UICollectionViewCell {
     @IBOutlet weak var thumbImg:UIImageView!
     @IBOutlet weak var nameLbl:UILabel!
     
-    var pokemon:Pokemon!
+    //var pokemon:Pokemon!
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        
         layer.cornerRadius=8
     }
     
     func configureCell(pokemon:Pokemon){
-        self.pokemon=pokemon
+//        self.pokemon=pokemon
+//        nameLbl.text=self.pokemon.name.capitalizedString
+//        thumbImg.image=UIImage(named: "\(self.pokemon.pokedexId)")
         
-        nameLbl.text=self.pokemon.name.capitalizedString
-        thumbImg.image=UIImage(named: "\(self.pokemon.pokedexId)")
+        nameLbl.text=pokemon.name.capitalizedString
+        thumbImg.image=UIImage(named: "\(pokemon.pokedexId)")
+
     }
     
 }
--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -12,10 +12,33 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
 
     @IBOutlet weak var collectionView:UICollectionView!
     
+    var pokemon=[Pokemon]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.delegate=self
         collectionView.dataSource=self
+        
+        parsePokemonCSV()
+    }
+    
+    func parsePokemonCSV(){
+        
+        let path=NSBundle.mainBundle().pathForResource("pokemon", ofType: "csv")!
+        
+        do{
+            let csv=try CSV(contentsOfURL: path )
+            let rows=csv.rows
+            for row in rows{
+                let pokeID=Int(row["id"]!)!
+                let name=row["identifier"]!
+                let poke=Pokemon(name: name, pokedexId: pokeID)
+                pokemon.append(poke)
+            }
+        }catch let err as NSError{
+            print(err.debugDescription)
+        }
+        
     }
     
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
@@ -29,8 +52,11 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         if let cell=collectionView.dequeueReusableCellWithReuseIdentifier("PokeCell", forIndexPath: indexPath) as? PokeCell{
             
-            let pokemon=Pokemon(name: "Pokemon",pokedexId: indexPath.row)
-            cell.configureCell(pokemon)
+            //let pokemon=Pokemon(name: "num:\(indexPath.row)",pokedexId: indexPath.row)
+            //cell.configureCell(pokemon)
+            
+            let poke=pokemon[indexPath.row]
+            cell.configureCell(poke)
             return cell
         }else{
             return UICollectionViewCell()
