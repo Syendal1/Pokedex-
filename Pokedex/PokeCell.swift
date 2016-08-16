0043dabb386958277076810bca51851d68e1b2f0
Syendal1 <snehith.yendal0313@gmail.com>
2016-08-15 22:34:30 -0700

    Cell Configuration

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by SnehithNitin on 8/15/16.
+//  Copyright © 2016 Snehith. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg:UIImageView!
+    @IBOutlet weak var nameLbl:UILabel!
+    
+    var pokemon:Pokemon!
+    
+    func configureCell(pokemon:Pokemon){
+        self.pokemon=pokemon
+        
+        nameLbl.text=self.pokemon.name.capitalizedString
+        thumbImg.image=UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
